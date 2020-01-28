485a4431c085c115ee5dd90733a105b20b42941d
Robert Svensk <Robertsvensk91@gmail.com>
2020-01-28 15:18:23 +0100

    [RE-START] using different tutorial to setup evironment

---
+++ README.md
@@ -0,0 +1,3 @@
+Tutorial being followed: blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+
+Don't forget to run source venv/bin/activate
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import instagram
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/robert")
 def robert():
