8a5b26ea66834b1084bdd46fa2327b66bb5a517e
Robert Svensk <Robertsvensk91@gmail.com>
2020-01-28 16:24:50 +0100

    Finished chapter 2 in tutorial

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,15 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username' : 'Robert'}
-    return render_template('index.html', title='Home', user=user)
+    
+    posts = [
+        {
+            'author': {'username': 'Cathrine'},
+            'body': 'Beautiful day in Linköping'
+        },
+        {
+            'author': {'username': 'Robert'},
+            'body': 'Alot of snow comming down in Cham now!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
