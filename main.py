c0b4f480efee1feb3bb784873855a35536583838
Robert Svensk <Robertsvensk91@gmail.com>
2020-01-30 17:33:57 +0100

    re-add of Salvador flask structure

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import instagram
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/plants")
+def plants():
+    return render_template("plants.html")
+
+@app.route("/robert")
+def robert():
+    return "Hello, Robert!"
+
+@app.route("/cathrine")
+def cathrine():
+    return "Tack så mycket Cathrine!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
