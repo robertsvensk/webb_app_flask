260cfe1b338fee8f5775046c7b5cfaf8967241c9
Robert Svensk <Robertsvensk91@gmail.com>
2020-02-02 15:39:56 +0100

    added login skeleton and water backbone

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import instagram
 
 app = Flask(__name__)
 
+WaterPlants = False
+def startWatering():
+    global WaterPlants
+    WaterPlants = True
+
+def waterPlants():
+    global WaterPlants
+    if (WaterPlants):
+        timer = 1000
+        while (timer > 0):
+            print(timer)
+        WaterPlants = False
+
+LoggedOn = False
+# Login handling
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", Login = LoggedOn)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", Login = LoggedOn)
 
-@app.route("/plants")
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    global LoggedOn
+    if request.method == "POST":
+        LoggedOn = True
+        return redirect(url_for("home"))
+    return render_template("login.html", Login = LoggedOn)
+
+ButtonPressed = 0
+@app.route("/plants", methods=['GET', 'POST'])
 def plants():
-    return render_template("plants.html")
+    global ButtonPressed
+    if request.method == "POST":
+        ButtonPressed += 1
+        return(redirect(url_for("water")))
+    return render_template("plants.html", Button = ButtonPressed, Login = LoggedOn)
+
+@app.route("/water")
+def water():
+    startWatering()
+    return(redirect(url_for('plants')))
 
 @app.route("/robert")
 def robert():
@@ -24,4 +58,5 @@ def cathrine():
     return "Tack så mycket Cathrine!"
 
 if __name__ == "__main__":
+    waterPlants()
     app.run(debug=True)
