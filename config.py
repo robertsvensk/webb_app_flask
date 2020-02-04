ad1d9b1f5a01e4b041378e6eecd5cf6d0ba6b616
Robert Svensk <Robertsvensk91@gmail.com>
2020-02-04 13:49:05 +0100

    correction of SQLAlchemy env_variable

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-nevern-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
             'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
