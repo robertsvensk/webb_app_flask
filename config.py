392fb4fce066e6585fb9cea7a311a21862569cd0
Robert Svensk <Robertsvensk91@gmail.com>
2020-02-15 16:05:09 +0100

    Added pages of posts in user profile as well

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
             'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
 
     # MAILSERVER SETTINGS
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
