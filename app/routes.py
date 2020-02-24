cd6f276800b1151121dec1be65bd693f0e1acf43
Robert Svensk <Robertsvensk91@gmail.com>
2020-02-24 15:21:11 +0100

    Basic webpage babel

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 #----------------------- LIB ----------------------#
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
+from flask import g
+from flask_babel import get_locale
 from werkzeug.urls import url_parse
 from datetime import datetime
 
@@ -164,6 +166,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
 
 @app.route('/follow/<username>')
 @login_required
