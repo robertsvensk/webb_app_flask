2971628f0a3aab43dcfb10ae0c03cae0bcb12466
Robert Svensk <Robertsvensk91@gmail.com>
2020-04-14 11:15:08 +0200

    elasticsearch index now updates on changes sql-db

--- app/search.py
+++ app/search.py
@@ -17,8 +17,8 @@ def query_index(index, query, page, per_page):
     if not current_app.elasticsearch:
         return [], 0
     search = current_app.elasticsearch.search(
-        index = index,
-        body = {'query': {'multi_match': {'query': query, 'fields:' ['*']}},
-                'from': (page - 1) * per_page, 'size': per_page})
+        index=index,
+        body={'query': {'multi_match': {'query': query, 'fields': ['*']}},
+              'from': (page - 1) * per_page, 'size': per_page})
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
     return ids, search['hits']['total']['value']
