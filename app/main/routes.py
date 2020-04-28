2829354ba49c5289c873a90e2b312c36d213afbe
Robert Svensk <Robertsvensk91@gmail.com>
2020-04-28 13:01:11 +0200

    included post form in explore forum view

--- app/main/routes.py
+++ app/main/routes.py
@@ -61,6 +61,18 @@ def forum():
 @bp.route('/explore')
 @login_required
 def explore():
+    form = PostForm()
+    if form.validate_on_submit():
+        language = guess_language(form.post.data)
+        if language == 'UKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
+        db.session.add(post)
+        db.session.commit()
+        flash(_('Your post is now live!'))
+        return redirect(url_for('main.explore'))
+
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
@@ -68,7 +80,7 @@ def explore():
         if posts.has_next else None
     prev_url = url_for('main.explore', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template("forum.html", title='Explore',
+    return render_template("forum.html", title='Explore', form=form,
                            posts=posts.items, next_url=next_url,
                            prev_url=prev_url)
 
